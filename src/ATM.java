2c8c450eed40fd5388d27e3f7089888840c587fa
zakoshgo <81923383+zakoshgo@users.noreply.github.com>
2022-05-18 10:45:58 +0200

    first commit

--- src/ATM.java
+++ src/ATM.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class ATM {
+//constructor
+//deposit
+//withdraw
+//show balance
+//account
+//.. user name .. password
+// atm will create account and then try to access it
+import java.util.*;
+
+public class ATM {
+    String username;
+    int password;
+    Account obj;
+    boolean createdAccountFlag = false;
+    boolean loginAccountFlag = false;
+    Scanner sc =  new Scanner(System.in);
+    void createAccount(String username,int password){
+        this.username = username;
+        this.password = password;
+
+        obj = new Account(username,password);
+        createdAccountFlag = true;
+    }
+
+
+    String login(String username , int password){
+
+        if(this.username == username && this.password == password && createdAccountFlag)
+        {
+            loginAccountFlag = true;
+            return "success";
+        }
+        else return "invalid username or password or you have to create account first";
+
+    }
+
+    String withdraw(int Amount){
+        if(loginAccountFlag){
+            return obj.withdraw(Amount);
+        }
+        else
+            return "you have to login first";
+    }
+
+    String deposit(int inputMoney){
+        if(loginAccountFlag){
+            return obj.deposit(inputMoney);
+        }
+        else
+            return "you have to login first";
+    }
+    String showBalance(){
+        if(loginAccountFlag){
+            return obj.showBalance();
+        }
+        else
+            return "you have to login first";
+    }
+
+
 }
--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Account {
+public class Account {
+    int balance;
+    String username;
+    int password;
+
+
+    Account(String username,int password){
+        this.username = username;
+        this.password = password;
+        balance =0;
+    }
+
+    String withdraw(int Amount){
+        if(Amount > balance){
+            return "their is not enough money dude";
+        }
+        balance -= Amount;
+        return "successful operation";
+    }
+
+    String deposit(int inputMoney){
+        if(inputMoney == 0){
+            return "you have to input money first man";
+        }
+        balance += inputMoney;
+        return "successful operation";
+    }
+    String showBalance(){
+        return String.valueOf(balance) ;
+    }
+
+
+
 }
